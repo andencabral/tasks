50818c7c2b8281d3a8ca91647af831300f81393a
Anden Cabral <anden.cabral@maine.edu>
2025-02-20 16:10:18 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,11 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                 UM COS420 with React Hooks and TypeScript
-                </header>
+                UM COS420 with React Hooks and TypeScript
+            </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Anden Cabral
-                
             </p>
         </div>
     );
